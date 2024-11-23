251c496b64c0ad30da045e0a2cbe7ee78d698345
sseker665 <sseker665@gmail.com>
2024-11-23 13:15:58 +0300

    added decision tree regression

---
+++ decision-tree-regression/decision-tree-regression.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 13:24:41 2024
+
+@author: junio
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("decision+tree+regression+dataset.csv",sep=";",header= None)
+
+x = df.iloc[:,0].values.reshape(-1,1)
+y = df.iloc[:,1].values.reshape(-1,1)
+
+# decision tree regression
+from sklearn.tree import DecisionTreeRegressor
+
+tree_reg = DecisionTreeRegressor()
+
+tree_reg.fit(x,y)
+
+tree_reg.predict([[6]])
+
+#visualize
+
+x_ = np.arange(min(x),max(x),0.01).reshape(-1,1)
+y_head = tree_reg.predict(x_)
+
+plt.scatter(x,y,color ="red")
+
+plt.plot(x_,y_head, color="green")
+plt.xlabel("tribun level")
+plt.ylabel("ucret")
+plt.show()
